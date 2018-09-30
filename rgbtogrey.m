499006cb94d224094dbef8cdde089efe71abc5ad
Akib Sadmanee <akibsadmanee@gmail.com>
2018-09-30 11:05:01 +0600

    rgbtogrey

---
+++ rgbtogrey.m
@@ -0,0 +1,12 @@
+function[]= rgbtogrey(RGB)
+    try
+        if exist(RGB, 'file') == 2
+           RGB=imread(RGB); 
+        end
+    catch
+    end
+    R=RGB(:,:,1);
+    G=RGB(:,:,2);
+    B=RGB(:,:,3);
+    img=0.299 * R + 0.587 * G + 0.114 * B;
+    imshow(img)
